ee8c335ed6d8f1ed3345e8c46f3c98cac663f5bc
Abror <izzatullaev2001abror@gmail.com>
2024-07-31 15:17:51 +0500

    improvements

--- app/user/models/user.py
+++
@@ -1,9 +0,0 @@
-from app.database import Base
-from sqlalchemy import Column, String, Integer
-
-
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(String(30), nullable=True, unique=True)
-    password = Column(String(255), nullable=False)
